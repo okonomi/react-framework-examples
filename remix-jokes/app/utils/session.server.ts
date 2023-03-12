be857ffeb341a939d742b6c6488f8c6fa6d3311e
okonomi <okonomi@oknm.jp>
2023-03-12 13:28:06 +0900

    implement getting logged in user id

--- remix-jokes/app/utils/session.server.ts
+++ remix-jokes/app/utils/session.server.ts
@@ -44,6 +44,34 @@ const storage = createCookieSessionStorage({
   },
 })
 
+async function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get("Cookie"))
+}
+
+export async function getUserId(request: Request) {
+  const session = await getUserSession(request)
+  const userId = session.get("userId")
+  if (!userId || typeof userId !== "string") {
+    return null
+  }
+  return userId
+}
+
+export async function requireUserid(
+  request: Request,
+  redirectTo: string = new URL(request.url).pathname
+) {
+  const session = await getUserSession(request)
+  const userId = session.get("userId")
+  if (!userId || typeof userId !== "string") {
+    const searchParams = new URLSearchParams([
+      ["redirectTo", redirectTo]
+    ])
+    throw redirect(`/login?${searchParams}`)
+  }
+  return userId
+}
+
 export async function createUserSession(
   userId: string,
   redirectTo: string
